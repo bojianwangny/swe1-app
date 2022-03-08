971ad4604cea83ad0d2d675a9ef0f97f0e537288
bojianwangny <bw2197@nyu.edu>
2022-03-08 18:34:08 -0500

    fix format

--- polls/models.py
+++ polls/models.py
@@ -1,12 +1,13 @@
-from django.db import models
 import datetime
+
 # Create your models here.
 from django.db import models
 from django.utils import timezone
 
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField("date published")
 
     def __str__(self):
         return self.question_text
